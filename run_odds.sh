58b3850344c743f60103aecbac13370ec6c5b96b
ivan-bogach <catmrgerman@gmail.com>
2021-01-02 13:30:29 +0300

    initial commit

--- run_odds.sh
+++ run_odds.sh
@@ -6,13 +6,20 @@ python train_tabnet.py \
        --csv-path data/odds.csv \
        --target-name "won" \
        --categorical-features sport,championat,league,E,status \
-       --feature_dim 13 \
-       --output_dim 13 \
+       --task classification
+       --val-frac 0.2 \
+       --test-frac 0.1 \
+       --model-name tabnet_adult_census \
+       --tb-log-location adult_census_logs \
+       --emb-size 1 \ 
+       --feature_dim 16 \
+       --output_dim 16 \
+       --n_steps 5 \
+       --lambda-sparsity 0.0001 \
+       --gamma 1.5 \
+       --lr 0.02 \
+       --batch-momentum 0.98 \
        --batch-size 4096 \
        --virtual-batch-size 128 \
-       --batch-momentum 0.98 \
-       --gamma 1.5 \
-       --n_steps 5 \
-       --decay-every 2500 \
-       --lambda-sparsity 0.0001 \
-       --max-steps 7700
+       --decay-every 500 \
+       --max-steps 3000
